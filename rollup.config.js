46303abf41cdc86691523dfa2f9edfe4b192ece5
baloran <contact@baloran.fr>
2021-02-18 15:49:34 +0100

    chore(rollup): es module

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,21 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from "rollup-plugin-terser";
  
 export default {
-    input: './src/index.ts',
-    output: {
-    file: 'dist/index.js',
-    format: 'cjs'
-  },
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
   plugins: [
     typescript(),
     terser()
